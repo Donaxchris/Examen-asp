25a0bade0918d5b65cd8b019ae6a29b8e01fe4ed
Moustapha LY <moustaphaly1996@gmail.com>
2024-08-19 21:41:00 +0000

    done

---
+++ Produits.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" Title="Produits" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Produits.aspx.cs" Inherits="Produits" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div> 
+        <asp:Label ID="Label1" runat="server" Text="Nom"></asp:Label>
+        <asp:TextBox ID="txtnom" runat="server"></asp:TextBox>
+        <br /><br />
+
+        <asp:Label ID="Label2" runat="server" Text="Description"></asp:Label>
+        <asp:TextBox ID="txtdescription" runat="server"></asp:TextBox>        
+        <br /><br />
+
+        <asp:Label ID="Label3" runat="server" Text="Categorie"></asp:Label>
+        <asp:TextBox ID="txtcategorie" runat="server"></asp:TextBox>
+         <br /><br />
+
+        <asp:Label ID="Label4" runat="server" Text="Couleur"></asp:Label>
+        <asp:TextBox ID="txtcouleur" runat="server"></asp:TextBox>
+        <br /><br />
+
+        <asp:Label ID="Label5" runat="server" Text="Genre"></asp:Label>
+        <asp:TextBox ID="txtgenre" runat="server"></asp:TextBox>
+        <br /><br />
+
+        <asp:Label ID="Label6" runat="server" Text="Taille"></asp:Label>
+        <asp:TextBox ID="txttaille" runat="server"></asp:TextBox>
+         <br /><br />
+
+        <asp:Label ID="Label7" runat="server" Text="Prix"></asp:Label>
+        <asp:TextBox  ID="txtprix" runat="server" TextMode="Number"></asp:TextBox>
+
+        <br /><br />
+
+        <asp:Label ID="lblmsg" runat="server" Text=""></asp:Label>
+        <br /><br />
+
+        <asp:Button ID="btnadd" runat="server" Text="Add" OnClick="btnadd_Click" />
+        <asp:Button ID="btnupdate" runat="server" Text="Update" OnClick="btnupdate_Click" />
+        <asp:Button ID="btndelete" runat="server" Text="Delete" OnClick="btndelete_Click" />
+        <br /><br />
+
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false">
+            <Columns>
+
+                <asp:BoundField DataField="nom" HeaderText="Nom" />
+                <asp:BoundField DataField="description" HeaderText="Description" />
+                <asp:BoundField DataField="categorie" HeaderText="Categorie" />
+                <asp:BoundField DataField="couleur" HeaderText="Couleur" />
+                <asp:BoundField DataField="genre" HeaderText="Genre" />
+                <asp:BoundField DataField="taille" HeaderText="Taille" />
+                <asp:BoundField DataField="prix" HeaderText="Prix" />
+
+                <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:LinkButton ID="lnkselect" runat="server" CommandArgument='<%# Eval("idart") %>' OnClick="lnkselect_Click" >Selectionner</asp:LinkButton>
+                    </ItemTemplate>
+                </asp:TemplateField>
+               
+            </Columns>
+
+        </asp:GridView>
+    </div>
+    <asp:SqlDataSource ID="Sample" runat="server" ConnectionString="Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\ecommerce.mdf;Integrated Security=True" ProviderName="System.Data.SqlClient" SelectCommand="SELECT * FROM [Article]"></asp:SqlDataSource>
+
+</asp:Content>
---
+++ Produits.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Xml.Linq;
+using System.Activities.Expressions;
+
+public partial class Produits : System.Web.UI.Page
+{
+    SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ecommerce"].ConnectionString);
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Session["currentUser"] = "User";
+        if (Session["currentUser"] == null)
+        {
+            Response.Redirect("Connexion.aspx");
+        }
+        if (!IsPostBack)
+        {
+            display();
+        }
+    }
+    public void display()
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from Article", con);
+        DataTable dt = new DataTable();
+        SqlDataAdapter adp = new SqlDataAdapter(cmd);
+        adp.Fill(dt);
+        GridView1.DataSource = dt;
+        GridView1.DataBind();
+        con.Close();
+    }
+
+    protected void btnadd_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        string str = "insert into article values('" + txtnom.Text + "','" + txtdescription.Text + "', '" + txtcategorie.Text + "', '" + txtcouleur.Text + "', '" + txtgenre.Text + "', '" + txttaille.Text + "', '" + txtprix.Text + "')";
+        SqlCommand cmd = new SqlCommand(str, con);
+        cmd.ExecuteNonQuery();
+        lblmsg.Text = "Record Inserted Successfully";
+        con.Close();
+        display();
+        cleartxt();
+    }
+
+    protected void lnkselect_Click(object sender, EventArgs e)
+    {
+        LinkButton btn = (LinkButton)sender;
+        Session["id"] = btn.CommandArgument;
+        con.Open();
+        SqlCommand cmd = new SqlCommand("select * from Article", con);
+        DataTable dt = new DataTable();
+        SqlDataAdapter adp = new SqlDataAdapter(cmd);
+        adp.Fill(dt);
+
+        if (dt.Rows.Count >= 0)
+        {
+            txtnom.Text = dt.Rows[0]["nom"].ToString();
+            txtdescription.Text = dt.Rows[0]["description"].ToString();
+            txtcategorie.Text = dt.Rows[0]["categorie"].ToString();
+            txtcouleur.Text = dt.Rows[0]["couleur"].ToString();
+            txtgenre.Text = dt.Rows[0]["genre"].ToString();
+            txttaille.Text = dt.Rows[0]["taille"].ToString();
+            txtprix.Text = dt.Rows[0]["prix"].ToString();
+
+        }
+        con.Close();
+    }
+
+    // fix here
+    protected void btnupdate_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        string str = "update Article set nom='" + txtnom.Text + "', description='" + txtdescription.Text + "', categorie='" + txtcategorie.Text + "', couleur='" + txtcouleur.Text + "', genre='" + txtgenre.Text + "', taille='" + txttaille.Text + "', prix='" + txtprix.Text + "' where idart='" + Session["id"] + "'";
+        SqlCommand cmd = new SqlCommand(str, con);
+        cmd.ExecuteNonQuery();
+        lblmsg.Text = "Record Updated Successfully";
+        con.Close();
+        display();
+    }
+
+    protected void btndelete_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        SqlCommand cmd = new SqlCommand("delete from tblemp where Article='" + Session["id"] + "'", con);
+        cmd.ExecuteNonQuery();
+        lblmsg.Text = "Record Deleted";
+        con.Close();
+        display();
+    }
+
+    public void cleartxt()
+    {
+        txtnom.Text = "";
+        txtdescription.Text = "";
+        txtcategorie.Text = "";
+        txtcouleur.Text = "";
+        txtgenre.Text = "";
+        txttaille.Text = "";
+        txtprix.Text = "";
+    }
+}
